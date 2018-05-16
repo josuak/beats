8b4d265690201a6355df61e54531d44b1bde81f7
Josua Krämer <kraemerjosua@gmail.com>
2018-05-16 17:32:48 +0200

    no more velocity js integration for slidetoggle

--- js/dropdown-box.js
+++ js/dropdown-box.js
@@ -6,21 +6,19 @@ $(function () {
   $(".dropdown-label").click(function (event) {
     //Save the clicked label in var. Since the event listener is "placed" on multiple elements with the same class we need to make sure to only refer to the one that actually got clicked -> event.target
     var elLabel = $(event.target),
-        elChevron = elLabel.children(".chevron"),
-				elContent = elLabel.next("");
+      elChevron = elLabel.children(".chevron"),
+			elContent = elLabel.next("");
     
     //use a build in jquery function to slide the content in and out of vision
-    //elContent.slideToggle();
+    elContent.slideToggle();
     
     //by changing the class on the chevron element it is flipped, we first check the state it is in and then flip it the other way around
     if (!elChevron.is(".up")) {
       elChevron.removeClass("down");
       elChevron.addClass("up");
-			elContent.velocity("slideDown", "easeInOut", 1000);
     } else {
       elChevron.removeClass("up");
       elChevron.addClass("down");
-			elContent.velocity("slideUp", "easeInOut", 1000);
     }
   });
   
@@ -29,7 +27,7 @@ $(function () {
     var elChevron = $(event.target),
 			elContent = elChevron.parent().next();
     
-    elContent.slideToggle(1000);
+    elContent.slideToggle();
     
     if (!elChevron.is(".up")) {
       elChevron.removeClass("down");
