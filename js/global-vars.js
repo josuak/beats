a4d9f049c483b7de400654e6d586215c184f69d5
Josua Krämer <kraemerjosua@gmail.com>
2018-06-17 22:23:45 +0200

    depreciated

--- js/global-vars.js
+++
@@ -1,7 +0,0 @@
-/*global $*/
-/* exported $navLi $navUl $indicator $sections preventScrollEvent*/
-
-var $navLi = $("nav ul li"),
-    $navUl = $("nav ul"),
-    $indicator = $("#pos-indicator"),
-    $sections = $("section"); 
