b9f36fe949067a029f70e61d54281246688c0928
Josua Krämer <kraemerjosua@gmail.com>
2019-04-09 04:09:51 +0200

    contact form spam protection

--- js/contact.js
+++ js/contact.js
@@ -1,17 +1,34 @@
-/* global $ loadjs */
+/* global $ loadjs grecaptcha */
 /* eslint require-jsdoc: 'off', no-invalid-this: 'off' */
 /* eslint-env browser */
 // loadjs.ready('jquery', () => {
-  // let preventSubmit = false;
-  const $validation = $('#validation');
-  const $userName = $('#user-name');
-  const $userEmail = $('#user-email');
-  // regex for checking email validity
-  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const $subject = $('#subject');
-  const $message = $('#message');
-  const $submit = $('#submit');
+// let preventSubmit = false;
+const $validation = $('#validation');
+const $userName = $('#user-name');
+const $userEmail = $('#user-email');
+// regex for checking email validity
+const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const $subject = $('#subject');
+const $message = $('#message');
+const $submit = $('#submit');
+let initalRender = false;
+function onloadCallback() {
   $('#contact form').on('submit', function(event) {
+    // run this function after all validations are completed
+    function verifyCallback(response) {
+      $validation.html('<span class="success-message">Sending...</span>');
+      // load contact php (ajax) and post values declared above
+      $validation.load('../php/contact.php', {
+        'user-name': userName,
+        'user-email': userEmail,
+        'subject': subject,
+        'message': message,
+        'submit': submit,
+        // post recaptcha respone for verification with google servers
+        'g-recaptcha-response': response,
+      });
+      $('.user-input').val('');
+    }
     // prevent form action upon submitting the form
     event.preventDefault();
     // get values from form inputs to post in contact php
@@ -47,32 +64,21 @@
       } else {
         $message.removeClass('input-error');
       }
-
     } else if (!re.test(userEmail)) {
       $validation.html('<span class="error-message">Seems like you\'ve entered an invalid email address.</span>');
       $userEmail.addClass('input-error');
     } else {
-      $validation.html('<span class="success-message">Sending...</span>');
-      // load contact php (ajax) and post values declared above
-      $validation.load('../php/contact.php', {
-        'user-name': userName,
-        'user-email': userEmail,
-        'subject': subject,
-        'message': message,
-        'submit': submit,
-      });
-      $('.user-input').val('');
-    /* SPAM BLOCK
-      preventSubmit = true;
-      setTimeout(function() {
-        preventSubmit = false;
-      }, 3000);
-    } else {
-      alert('don\'t spam bruh');
-      // <span class="error-message">Please fill out all fields.</span>
-    }*/
-  }
-});
+      $validation.html('<span class="success-message">Complete the reCAPTCHA to send your message.</span>');
+      if(initalRender === false) {
+        grecaptcha.render('recaptcha', {
+          'sitekey': '6Lec_5wUAAAAAHYKbXClfn5ODWfM2eH1IvuE0dLp',
+          'callback': verifyCallback,
+          'theme': 'dark',
+        });
+      }
+    }
+  });
+}
 $('.user-input').on('change', function() {
     $(this).removeClass('input-error');
 });
